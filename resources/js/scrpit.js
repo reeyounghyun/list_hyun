6bf2c8cabf7b87d077080db69f3659a5b95eced0
LAPTOP-QN1IRFV6\이쁜이 <naem77716@gmail.com>
2023-12-24 22:41:24 +0900

    실시간 미리보기

--- resources/js/scrpit.js
+++ resources/js/scrpit.js
@@ -6,4 +6,5 @@ $(document).ready(function () {
 function common_load() {
 	$('.header').load('header.html .header .home_nav', function () {});
 	$('.footer').load('footer.html .footer .footer_wrap', function () {});
+	$('.Home_wrap_img').load('home.html .Home_wrap_img', function () {});
 }
