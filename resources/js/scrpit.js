966074b8cb2cd9db29a7f13b7a44933a8cdd77dd
LAPTOP-QN1IRFV6\이쁜이 <naem77716@gmail.com>
2023-12-24 20:08:11 +0900

    푸터 적용

--- resources/js/scrpit.js
+++ resources/js/scrpit.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 	common_load(); // 문서로드
 });
 
-// 헤더로드
+// 헤더&푸터 로드
 function common_load() {
 	$('.header').load('header.html .header .home_nav', function () {});
+	$('.footer').load('footer.html .footer .footer_wrap', function () {});
 }
