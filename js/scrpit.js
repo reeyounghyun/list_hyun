cd02cfc768abcba2362e04f19a12c054a18f963a
LAPTOP-QN1IRFV6\이쁜이 <naem77716@gmail.com>
2024-01-14 18:03:42 +0900

    포트폴리오 수정작업

--- js/scrpit.js
+++ js/scrpit.js
@@ -8,3 +8,24 @@ function common_load() {
 	$('.Home_wrap_img').load('index.html .Home_wrap_img', function () {});
 	$('.footer').load('footer.html .footer .footer_wrap', function () {});
 }
+
+function togglePopup(popupId) {
+	var popup = document.getElementById(popupId);
+	var overlay = document.getElementById('overlay');
+	if (popup.style.display === 'block') {
+		popup.style.display = 'none';
+		overlay.style.display = 'none';
+	} else {
+		popup.style.display = 'block';
+		overlay.style.display = 'block';
+	}
+}
+
+function closePopup() {
+	var popups = document.querySelectorAll('.popup');
+	var overlay = document.getElementById('overlay');
+	popups.forEach(function (popup) {
+		popup.style.display = 'none';
+	});
+	overlay.style.display = 'none';
+}
